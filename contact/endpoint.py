c93ac410ffa62cd0ad13d6f37a3a9d5fbfb36bd1
zeeshan52123 <iffi7604@gmail.com>
2023-09-13 09:09:30 -0700

    changes

--- contact/endpoint.py
+++ contact/endpoint.py
@@ -7,37 +7,37 @@ from email.mime.multipart import MIMEMultipart
 router= APIRouter()
 
 
-# Replace these with your email server and credentials
+# Replace email server and credentials
 SMTP_SERVER = "your-smtp-server.com"
 SMTP_PORT = 587
-SMTP_USERNAME = "your-email@example.com"
-SMTP_PASSWORD = "your-email-password"
+SMTP_USERNAME = "email"
+SMTP_PASSWORD = "passwordS"
 
-# Define an endpoint to send emails
+# endpoint send emails
 @router.post("/send-email/")
 async def send_email(email_request: EmailRequest):
     try:
-        # Create an SMTP connection
+        #SMTP connection
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
         server.starttls()
         server.login(SMTP_USERNAME, SMTP_PASSWORD)
 
-        # Create the email message
+        #email message
         msg = MIMEMultipart()
         msg['From'] = SMTP_USERNAME
         msg['To'] = email_request.email
         msg['Subject'] = "Discussion: " + email_request.discussion
 
-        # Add the email content
+        #email content
         body = f"Dear {email_request.first_name} {email_request.last_name},\n\n"
         body += f"Discussion: {email_request.discussion}\n\n"
         body += f"Comments: {email_request.comments}\n\n"
         msg.attach(MIMEText(body, 'plain'))
 
-        # Send the email
+        # Send email
         server.sendmail(SMTP_USERNAME, email_request.email, msg.as_string())
 
-        # Close the SMTP connection
+        # Close SMTP connection
         server.quit()
 
         return {"message": "Email sent successfully"}
--- information/endpoint1.py
+++ information/endpoint1.py
@@ -7,28 +7,28 @@ from email.mime.multipart import MIMEMultipart
 router1= APIRouter()
 
 
-# Replace these with your email server and credentials
+# Replace email server and credentials
 SMTP_SERVER = "your-smtp-server.com"
 SMTP_PORT = 587
 SMTP_USERNAME = "your-email@example.com"
 SMTP_PASSWORD = "your-email-password"
 
-# Define an endpoint to send emails
+# endpoint to send emails
 @router1.post("/Information/")
 async def send_email(email_request: EmailRequest):
     try:
-        # Create an SMTP connection
+        #SMTP connection
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
         server.starttls()
         server.login(SMTP_USERNAME, SMTP_PASSWORD)
 
-        # Create the email message
+        #email message
         msg = MIMEMultipart()
         msg['From'] = SMTP_USERNAME
         msg['To'] = email_request.email
         msg['Subject'] = "mesage: " + email_request.message
 
-        # Add the email content
+        #email content
         body = f"Dear {email_request.first_name} {email_request.last_name},\n\n"
         body += f"message: {email_request.message}\n\n"
         body += f"Phone: {email_request.phone}\n\n"
@@ -37,10 +37,10 @@ async def send_email(email_request: EmailRequest):
         body += f"Message: {email_request.message}\n\n"
         msg.attach(MIMEText(body, 'plain'))
 
-        # Send the email
+        #Send email
         server.sendmail(SMTP_USERNAME, email_request.email, msg.as_string())
 
-        # Close the SMTP connection
+        #Close SMTP connection
         server.quit()
 
         return {"message": "Email sent successfully"}
